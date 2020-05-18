fb56aa14e146cda724bb4d702826ddf16498dd64
Mayank Srivastava <mayanksrivastava9038@gmail.com>
2020-05-18 08:49:13 +0100

    files

---
+++ target/classes/data.sql
@@ -0,0 +1,15 @@
+insert into vehicle values (100000,'Ford','Focus','Style 1400cc',40.12);
+insert into vehicle values (100001,'Mercedes','Benz','CLA 250e',50.12);
+insert into vehicle values (100002,'Mercedes','Benz','GLA 250e',52.12);
+insert into vehicle values (100003,'Mercedes','Benz','GLA B 250e',55.12);
+insert into vehicle values (100004,'Mercedes','Benz','GLA B 250e',55.12);
+
+
+insert into options values (100000,'sunroof',42,1);
+insert into options values (100001,'heated seat',45, 1);
+insert into options values (100002,'air-con',48, 1);
+
+
+
+
+
